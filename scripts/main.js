7f5c54a704c9fc2faa6b674a6ce7a6cbca5795e4
9ycountry6 <9ycountry6@gmail.com>
2026-06-07 08:02:55 +0000

    Update project files and sync to June 7th repository

--- scripts/main.js
+++ scripts/main.js
@@ -16,32 +16,3 @@ function showSlides() {
     setTimeout(showSlides, 5000); // 5초마다 이미지 변경
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const dropdowns = document.querySelectorAll('.dropdown');
-
-    dropdowns.forEach(dropdown => {
-        const dropbtn = dropdown.querySelector('.dropbtn');
-        const dropdownContent = dropdown.querySelector('.dropdown-content');
-
-        dropbtn.addEventListener('click', function (event) {
-            event.preventDefault();
-            const isVisible = dropdownContent.style.display === 'block';
-            // Close all dropdowns
-            document.querySelectorAll('.dropdown-content').forEach(content => {
-                content.style.display = 'none';
-            });
-            // Open the clicked dropdown if it was not visible
-            if (!isVisible) {
-                dropdownContent.style.display = 'block';
-            }
-        });
-    });
-
-    window.addEventListener('click', function (event) {
-        if (!event.target.matches('.dropbtn')) {
-            document.querySelectorAll('.dropdown-content').forEach(content => {
-                content.style.display = 'none';
-            });
-        }
-    });
-});
